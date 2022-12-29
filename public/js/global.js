febe28b4c8569914d4016058a4e714d9812ab3a5
trvinhky <vinhky26032002@gmail.com>
2022-12-29 22:01:34 +0700

    update file html

--- public/js/global.js
+++ public/js/global.js
@@ -1,4 +1,17 @@
-$(".navbar-item__link").click(function () {
-  $(".navbar-item__link").removeClass("active");
-  $(this).addClass("active");
+$(document).ready(() => {
+  $(".navbar-item__link").click(function () {
+    $(".navbar-item__link").removeClass("active");
+    $(this).addClass("active");
+  });
+
+  function handleScroll() {
+    const scrollTop = $(this).scrollTop();
+    if (scrollTop > 50) {
+      $(".header-navbar").addClass("scroll");
+    } else {
+      $(".header-navbar").removeClass("scroll");
+    }
+  }
+
+  $(window).on("scroll", handleScroll);
 });
